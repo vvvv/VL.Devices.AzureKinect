05cdfd2cad79b070863216e358d3841495af7962
natan-sinigaglia <natan.sinigaglia@gmail.com>
2022-09-23 14:45:37 +0200

    References the SDK directly as a submodule

---
+++ src/VL.Devices.AzureKinect/KinectImageExtensions.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.Azure.Kinect.Sensor;
+using System;
+using System.Reactive.Linq;
+using VL.Lib.Basics.Imaging;
+using VL.Lib.Collections;
+using Stride.Core.Mathematics;
+
+namespace VL.Devices.AzureKinect
+{
+    public static class KinectImageExtensions
+    {
+        struct XYZ
+        {
+            public short x;
+            public short y;
+            public short z;
+        }
+
+        /// <summary>
+        /// Tries to decompress the image. Will return true if the image needed to be decompressed and therefor should get disposed of by the caller.
+        /// </summary>
+        /// <param name="image">The image which might be in a compressed form.</param>
+        /// <param name="decompressedImage">The decompressed image if decrompression was necessary or the original image.</param>
+        /// <returns>True if decompression was neccessary.</returns>
+        public static bool TryToDecompress(this Image image, out Image decompressedImage)
+        {
+            switch (image.Format)
+            {
+                case ImageFormat.ColorMJPG:
+                    decompressedImage = image.Decompress();
+                    return true;
+                default:
+                    decompressedImage = image;
+                    return false;
+            }
+        }
+
+        /// <summary>
+        /// Selects the color images out of the captures. Will also decompress them if needed.
+        /// </summary>
+        /// <param name="captures">The captures to select the color images from.</param>
+        /// <returns>The color images of the captures.</returns>
+        public static IObservable<IImage> SelectColorImages(this IObservable<Capture> captures)
+        {
+            return captures.SelectMany(c =>
+            {
+                var image = c.Color;
+                if (image != null)
+                {
+                    if (image.TryToDecompress(out var decompressedImage))
+                        return Observable.Using(() => decompressedImage, i => Observable.Return(i.AsVLImage()));
+                    else
+                        return Observable.Return(image.AsVLImage());
+                }
+                return Observable.Empty<IImage>();
+            });
+        }
+
+        /// <summary>
+        /// Selects the depth images out of the captures.
+        /// </summary>
+        /// <param name="captures">The captures to select the depth images from.</param>
+        /// <returns>The depth images of the captures.</returns>
+        public static IObservable<IImage> SelectDepthImages(this IObservable<Capture> captures)
+        {
+            return captures.SelectMany(c =>
+            {
+                var image = c.Depth;
+                if (image != null)
+                {
+                    return Observable.Return(image.AsVLImage());
+                }
+                return Observable.Empty<IImage>();
+            });
+        }
+
+        /// <summary>
+        /// Selects the infrared images out of the captures.
+        /// </summary>
+        /// <param name="captures">The captures to select the infrared images from.</param>
+        /// <returns>The infrared images of the captures.</returns>
+        public static IObservable<IImage> SelectInfraredImages(this IObservable<Capture> captures)
+        {
+            return captures.SelectMany(c =>
+            {
+                var image = c.IR;
+                if (image != null)
+                {
+                    return Observable.Return(image.AsVLImage());
+                }
+                return Observable.Empty<IImage>();
+            });
+        }
+
+        /// <summary>
+        /// Since a capture can miss either the color or depth image this operator will take care of always producing
+        /// a capture where the latest received color and depth images are set.
+        /// </summary>
+        public static IObservable<Capture> CombineLatestImages(this IObservable<Capture> captures)
+        {
+            var latestColor = default(Image);
+            var latestDepth = default(Image);
+            var latestIR = default(Image);
+            return captures.SelectMany(c =>
+            {
+                if (c.Color != null)
+                {
+                    latestColor?.Dispose();
+                    latestColor = c.Color.Reference();
+                }
+                if (c.Depth != null)
+                {
+                    latestDepth?.Dispose();
+                    latestDepth = c.Depth.Reference();
+                }
+                if (c.IR != null)
+                {
+                    latestIR?.Dispose();
+                    latestIR = c.IR.Reference();
+                }
+                if (latestColor != null || latestDepth != null || latestIR != null)
+                {
+                    return Observable.Using(
+                        () => CreateCapture(latestColor?.Reference(), latestDepth?.Reference(), latestIR?.Reference(), c.Temperature),
+                        x => Observable.Return(x));
+                }
+                return Observable.Empty<Capture>();
+            });
+        }
+
+        public static IObservable<Capture> DecompressColorImages(this IObservable<Capture> captures)
+        {
+            return captures.SelectMany(c =>
+            {
+                var colorImage = c.Color;
+                if (colorImage != null && colorImage.TryToDecompress(out var decompressedImage))
+                    return Observable.Using(
+                        () => CreateCapture(decompressedImage, c.Depth?.Reference(), c.IR?.Reference(), c.Temperature), 
+                        x => Observable.Return(x));
+                else
+                    return Observable.Return(c);
+            });
+        }
+
+        private static Capture CreateCapture(Image color, Image depth, Image ir, float temperature)
+        {
+            var capture = new Capture() { Temperature = temperature };
+            // The setters crash when assigning null
+            if (color != null)
+                capture.Color = color;
+            if (depth != null)
+                capture.Depth = depth;
+            if (ir != null)
+                capture.IR = ir;
+            return capture;
+        }
+
+        public static IImage AsVLImage(this Image image)
+        {
+            ReadOnlyMemory<byte> memory = image.Memory;
+            return memory.ToImage(image.WidthPixels, image.HeightPixels, image.Format.ToPixelFormat(), image.Format.ToString(), isVolatile: true);
+        }
+
+        public static Spread<Vector3> GetPointCloudData(this Image image, float scaling = 1f)
+        {
+            if (image.Format != ImageFormat.Custom)
+                throw new UnsupportedImageFormatException(image.Format);
+
+            var data = image.GetPixels<XYZ>();
+            var result = new SpreadBuilder<Vector3>(data.Length);
+            foreach (var pixel in data.Span)
+            {
+                result.Add(new Vector3(pixel.x * scaling, pixel.y * scaling, pixel.z * scaling));
+            }
+            return result.ToSpread();
+        }
+
+        public static IImage PointCloudeImageToBGRAImage(this Image image, float scaling = 1f)
+        {
+            if (image.Format != ImageFormat.Custom)
+                throw new UnsupportedImageFormatException(image.Format);
+
+            var data = image.GetPixels<XYZ>();
+            var pixels = new ColorBGRA[data.Length];
+            var i = 0;
+            foreach (var pixel in data.Span)
+            {
+                pixels[i++] = new ColorBGRA(pixel.x * scaling, pixel.y * scaling, pixel.z * scaling, 1);
+            }
+            return pixels.ToImage(image.WidthPixels, image.HeightPixels, PixelFormat.B8G8R8A8);
+        }
+
+        public static PixelFormat ToPixelFormat(this ImageFormat format)
+        {
+            switch (format)
+            {
+                case ImageFormat.ColorBGRA32:
+                    return PixelFormat.B8G8R8A8;
+                case ImageFormat.Depth16:
+                    return PixelFormat.R16;
+                case ImageFormat.IR16:
+                    return PixelFormat.R16;
+                case ImageFormat.Custom16:
+                    return PixelFormat.R16;
+                case ImageFormat.Custom8:
+                    return PixelFormat.R8;
+                default:
+                    throw new UnsupportedImageFormatException(format);
+            }
+        }
+    }
+}
---
+++ src/VL.Devices.AzureKinect/PointCloudUtils.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.Kinect.Sensor;
+using VL.Lib.Collections;
+using System.Numerics;
+using System.Linq;
+
+namespace VL.Devices.AzureKinect
+{
+    public static class PointCloudUtils
+    {
+        class ExtrinsicsComparer : IEqualityComparer<Extrinsics>
+        {
+            public static readonly ExtrinsicsComparer Default = new ExtrinsicsComparer();
+
+            public bool Equals(Extrinsics x, Extrinsics y)
+            {
+                return x.Rotation.SequenceEqual(y.Rotation) &&
+                       x.Translation.SequenceEqual(y.Translation);
+            }
+
+            public int GetHashCode(Extrinsics obj)
+            {
+                return 0;
+            }
+        }
+
+        class IntrinsicsComparer : IEqualityComparer<Intrinsics>
+        {
+            public static readonly IntrinsicsComparer Default = new IntrinsicsComparer();
+
+            public bool Equals(Intrinsics x, Intrinsics y)
+            {
+                return x.Type == y.Type &&
+                       x.ParameterCount == y.ParameterCount &&
+                       // The last 4 values are very very small so let's ignore them as it would cause equality check to fail
+                       x.Parameters.Take(12).SequenceEqual(y.Parameters.Take(12));
+            }
+
+            public int GetHashCode(Intrinsics obj)
+            {
+                return 0;
+            }
+        }
+
+        class CameraCalibrationComparer : IEqualityComparer<CameraCalibration>
+        {
+            public static readonly CameraCalibrationComparer Default = new CameraCalibrationComparer();
+
+            public bool Equals(CameraCalibration x, CameraCalibration y)
+            {
+                return x.ResolutionWidth == y.ResolutionWidth &&
+                       x.ResolutionHeight == y.ResolutionHeight &&
+                       x.MetricRadius == y.MetricRadius &&
+                       ExtrinsicsComparer.Default.Equals(x.Extrinsics, y.Extrinsics) &&
+                       IntrinsicsComparer.Default.Equals(x.Intrinsics, y.Intrinsics);
+
+            }
+
+            public int GetHashCode(CameraCalibration obj)
+            {
+                return 0;
+            }
+        }
+        class CalibrationComparer : IEqualityComparer<Calibration>
+        {
+            public static readonly CalibrationComparer Default = new CalibrationComparer();
+
+            public bool Equals(Calibration x, Calibration y)
+            {
+                return x.ColorResolution == y.ColorResolution &&
+                       x.DepthMode == y.DepthMode &&
+                       CameraCalibrationComparer.Default.Equals(x.ColorCameraCalibration, y.ColorCameraCalibration) &&
+                       CameraCalibrationComparer.Default.Equals(x.DepthCameraCalibration, y.DepthCameraCalibration) &&
+                       x.DeviceExtrinsics.SequenceEqual(y.DeviceExtrinsics, ExtrinsicsComparer.Default);
+            }
+
+            public int GetHashCode(Calibration obj)
+            {
+                return obj.ColorResolution.GetHashCode();
+            }
+        }
+
+        static Dictionary<Calibration, Spread<Stride.Core.Mathematics.Vector2>> depthRayTableCache = 
+            new Dictionary<Calibration, Spread<Stride.Core.Mathematics.Vector2>>(CalibrationComparer.Default);
+        public static Spread<Stride.Core.Mathematics.Vector2> BuildDepthRayTable(Calibration calibration)
+        {
+            if (depthRayTableCache.TryGetValue(calibration, out var rayTable))
+                return rayTable;
+
+            int width = calibration.DepthCameraCalibration.ResolutionWidth;
+            int height = calibration.DepthCameraCalibration.ResolutionHeight;
+
+            var table_data = new Stride.Core.Mathematics.Vector2[width * height];// xy_table. (k4a_float2_t *)(void*)k4a_image_get_buffer(xy_table);
+
+            Vector2 p;
+            for (int y = 0, idx = 0; y < height; y++)
+            {
+                p.Y = y;
+                for (int x = 0; x < width; x++, idx++)
+                {
+                    p.X = x;
+                    var ray = calibration.TransformTo3D(p, 1, CalibrationDeviceType.Depth, CalibrationDeviceType.Depth);
+
+                    if (ray.HasValue)
+                    {
+                        table_data[idx].X = ray.Value.X;
+                        table_data[idx].Y = -ray.Value.Y;
+                    }
+                    else
+                    {
+                        table_data[idx].X = float.NaN;
+                        table_data[idx].Y = float.NaN;
+                    }
+                }
+            }
+
+            rayTable = table_data.ToSpread();
+            depthRayTableCache[calibration] = rayTable;
+            return rayTable;
+        }
+
+        static Dictionary<Calibration, Spread<Stride.Core.Mathematics.Vector2>> colorRayTableCache =
+            new Dictionary<Calibration, Spread<Stride.Core.Mathematics.Vector2>>(CalibrationComparer.Default);
+        public static Spread<Stride.Core.Mathematics.Vector2> BuildColorRayTable(Calibration calibration)
+        {
+            if (colorRayTableCache.TryGetValue(calibration, out var rayTable))
+                return rayTable;
+
+            int width = calibration.ColorCameraCalibration.ResolutionWidth;
+            int height = calibration.ColorCameraCalibration.ResolutionHeight;
+
+            var table_data = new Stride.Core.Mathematics.Vector2[width * height];// xy_table. (k4a_float2_t *)(void*)k4a_image_get_buffer(xy_table);
+
+            Vector2 p;
+            for (int y = 0, idx = 0; y < height; y++)
+            {
+                p.Y = y;
+                for (int x = 0; x < width; x++, idx++)
+                {
+                    p.X = x;
+                    var ray = calibration.TransformTo3D(p, 1, CalibrationDeviceType.Color, CalibrationDeviceType.Color);
+
+                    if (ray.HasValue)
+                    {
+                        table_data[idx].X = ray.Value.X;
+                        table_data[idx].Y = -ray.Value.Y;
+                    }
+                    else
+                    {
+                        table_data[idx].X = float.NaN;
+                        table_data[idx].Y = float.NaN;
+                    }
+                }
+            }
+
+            rayTable = table_data.ToSpread();
+            colorRayTableCache[calibration] = rayTable;
+            return rayTable;
+        }
+    }
+}
