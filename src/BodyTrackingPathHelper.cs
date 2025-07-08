be9f92a758489f4ac20f77f2d2baac2fe058d987
joreg <joreg@vvvv.org>
2025-07-08 12:48:00 +0200

    merged with VL.Devices.AzureKinect.Body

---
+++ src/BodyTrackingPathHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace VL.Devices.AzureKinect.Body
+{
+    public static class PathHelper
+    {
+        [System.Runtime.InteropServices.DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        private static extern void SetDllDirectory(string lpPathName);
+
+        public static void SetRequiredDLLPaths()
+        {
+            var dllPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var directMLPath = Path.Combine(dllPath, @"..\..\runtimes\win-x64\native");
+            SetDllDirectory(directMLPath);
+
+            var cudaPath = Environment.GetEnvironmentVariable("CUDA_PATH");
+            if (!string.IsNullOrEmpty(cudaPath))
+            {
+                cudaPath = Path.Combine(cudaPath, "bin");
+                SetDllDirectory(cudaPath);
+            }
+
+            var cudnnPath = @"C:\Program Files\Azure Kinect Body Tracking SDK\tools";
+            if (Directory.Exists(cudnnPath))
+                SetDllDirectory(cudnnPath);
+        }
+    }
+}
